9d604b541b2e20d9b919e6a1a6f0753e1a871db5
Ahmed Al-Majidi <af774857999@gmail.com>
2020-09-13 12:58:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const returnFirstTwoDrivers = function(arr) {
   let a =[];
     for (var i = 0; i < 2; i++) {
-      // a.push(arr[i]);
-      a[i] = arr[i];
+      a.push(arr[i]);
+      // a[i] = arr[i];
     }
     return a;
 }
