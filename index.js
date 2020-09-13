649056e7e912c033015d9d68bbb0bf56ee48b881
Ahmed Al-Majidi <af774857999@gmail.com>
2020-09-13 12:53:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file!
+const fn1 = function(arr) {
+  let a =[]
+    for (var i = 0; i < 2; i++) {
+      // a.push(arr[i]);
+      a[i] = arr[i];
+    }
+    return a;
+}
