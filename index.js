90c29dd1711ce40001d692385f145b6b79a315db
Ahmed Al-Majidi <af774857999@gmail.com>
2020-09-13 12:57:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const fn1 = function(arr) {
-  let a =[]
+const returnFirstTwoDrivers = function(arr) {
+  let a =[];
     for (var i = 0; i < 2; i++) {
       // a.push(arr[i]);
       a[i] = arr[i];
     }
     return a;
 }
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+returnFirstTwoDrivers(drivers)
